bd5cb9eb2f53229c1acfa7c1e5f661a330a6ee7e
Chen <2641635718@qq.com>
2017-03-27 16:42:50 +0800

    First commit

---
+++ app/src/main/java/com/example/lenovo/book/WelcomeActivity.java
@@ -0,0 +1,30 @@
+package com.example.lenovo.book;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Handler;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_welcome);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                finish();
+
+            }
+        }, 500);
+    }
+}
